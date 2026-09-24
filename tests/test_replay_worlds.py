80cfc139ebe40ea1dd23b8e5c40581690cbcc00d
Laura <laurajoyhutchins@gmail.com>
2026-09-23 23:32:50 -0600

    Remove obsolete replay probe test

--- tests/test_replay_worlds.py
+++ tests/test_replay_worlds.py
@@ -10,7 +10,6 @@ from azelficoast.replay_worlds import (
     ReplayError,
     ReplayEvidence,
     build_replay_belief,
-    collect_observations,
     condition_generator_prior_on_public_history,
     damage_rolls_for_item,
     extract_damage_observations,
@@ -85,27 +84,6 @@ def test_replay_json_url_strips_viewpoint_query() -> None:
     assert replay_json_url(
         "https://replay.pokemonshowdown.com/gen9randombattle-1-secret?p2"
     ) == "https://replay.pokemonshowdown.com/gen9randombattle-1-secret.json"
-
-
-def test_protocol_observations_follow_active_species() -> None:
-    log = """|turn|1
-|switch|p1a: Ape|Infernape, L82|100/100
-|switch|p2a: Gambit|Kingambit, L74|100/100
-|move|p1a: Ape|Close Combat|p2a: Gambit
-|-damage|p2a: Gambit|79/100
-|turn|2
-|move|p1a: Ape|Flare Blitz|p2a: Gambit
-|-enditem|p1a: Ape|Choice Scarf
-"""
-    observations = collect_observations(parse_protocol(log))
-
-    infernape = observations[("p1", "Infernape")]
-    assert infernape.level == 82
-    assert infernape.moves == [(1, "Close Combat"), (2, "Flare Blitz")]
-    assert infernape.item_events == [(2, "-enditem", "Choice Scarf")]
-
-    kingambit = observations[("p2", "Kingambit")]
-    assert kingambit.hp_events == [(1, "79/100")]
 
 
 def test_extracts_exact_public_damage_and_tera_state() -> None:
