e95bd50fa7b5a305488fbc6f2d21ad2895e9d7c6
Laura <laurajoyhutchins@gmail.com>
2026-09-26 08:33:32 -0600

    Make the CJS script frontier statically checked

---
+++ scripts/node-globals.d.ts
@@ -0,0 +1,14 @@
+interface AzelficoastNodeRequire {
+  (id: string): any;
+  main?: any;
+}
+
+declare const Buffer: any;
+declare const __dirname: string;
+declare const __filename: string;
+declare const module: any;
+declare const process: any;
+declare const require: AzelficoastNodeRequire;
+
+declare function clearImmediate(handle: any): void;
+declare function setImmediate(callback: (...args: any[]) => void, ...args: any[]): any;
